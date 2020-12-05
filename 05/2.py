8e24ac184ff0ed79443a51f36bfdd4c6dbd0d141
LarsVomMars <lars@kroenner.eu>
2020-12-05 06:48:40 +0100

    05

---
+++ 05/2.py
@@ -0,0 +1,41 @@
+rows = open("input", "r").readlines()
+
+sids = []
+
+for row in rows:
+    row = row.strip()
+
+    # row = "FBFBBFFRLR"
+    srow = row[:7]
+    seat = row[7:]
+
+    b = 0
+    t = 127
+    for rc in srow:
+        d = ((t - b) // 2) +1
+        if rc == "F":
+            t -= d
+        elif rc == "B":
+            b += d
+
+    nrow = b
+
+    b = 0
+    t = 7
+    for rc in seat:
+        d = ((t - b) // 2) +1
+        if rc == "L":
+            t -= d
+        elif rc == "R":
+            b += d
+
+
+    nseat = b
+
+    sids.append(nrow * 8 + nseat)
+
+sids = sorted(sids)
+            
+for i in range(1, len(sids) - 1):
+    if not (sids[i-1] + 1 == sids[i] == sids[i+1] - 1):
+        print(i, sids[i-1], sids[i], sids[i+1]) # Therefore, the solution is the missing number in the two outputted lines
