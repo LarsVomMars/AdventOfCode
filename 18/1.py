75bec84d2870b31e1de51ca548fd18b7a67eecb8
LarsVomMars <developer@kroenner.eu>
2020-12-18 14:04:52 +0100

    18
    Don't wanna talk about it

---
+++ 18/1.py
@@ -0,0 +1,39 @@
+import operator
+rows = [row.strip() for row in open("input", "r").readlines()]
+
+OPS = {
+    "+": operator.add,
+    "*": operator.mul,
+}
+
+
+def get_para_idx(toks):
+    opn = 1
+    for i, e in enumerate(toks):
+        opn += 1 if e == "(" else -1 if e == ")" else 0
+        if opn == 0:
+            return i
+
+
+def calc(toks):
+    res = 0
+    op = "+"
+    while len(toks) > 0:
+        tk = toks.pop(0)
+        if tk in "(":
+            idx = get_para_idx(toks)
+            res = OPS[op](res, calc(toks[:idx+1]))
+            toks = toks[idx:]
+        elif tk.isdigit():
+            res = OPS[op](res, int(tk))
+        elif tk in OPS.keys():
+            op = tk
+    return res
+
+
+s = 0
+for row in rows:
+    row = row.replace("(", "( ").replace(")", " )")
+    toks = row.split()
+    s += calc(toks)
+print(s)
---
+++ 18/2.py
@@ -0,0 +1,27 @@
+rows = [row.strip() for row in open("input", "r").readlines()]
+
+
+def get_para_idx(toks):
+    opn = 1
+    for i, e in enumerate(toks):
+        opn += 1 if e == "(" else -1 if e == ")" else 0
+        if opn == 0:
+            return i
+
+
+def calc(toks):
+    while "(" in toks:  # Recursive wohoo
+        i = toks.index("(")
+        ci = get_para_idx(toks[i+1:]) + i + 1
+        toks = toks[:i] + str(calc(toks[i+1:ci])) + toks[ci+1:]
+    toks = toks.split()
+    while "+" in toks:  # Apply + before *
+        i = toks.index("+")
+        toks[i-1:i+2] = [f"{int(toks[i-1]) + int(toks[i+1])}"]
+    return eval(" ".join(toks))  # Eval wohoo
+
+
+s = 0
+for row in rows:
+    s += calc(row)
+print(s)
