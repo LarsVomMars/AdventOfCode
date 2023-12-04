28f899d7fc7ed837585999a081a5b6a059c2d353
LarsVomMars <developer@kroenner.eu>
2023-12-04 06:26:21 +0100

    04

---
+++ 2023/04/main.py
@@ -0,0 +1,32 @@
+import re
+
+LINES = [line.strip() for line in open("input").readlines()]
+
+reg = re.compile(r"\d+")
+
+GAMES = []
+for line in LINES:
+    game, nums = line.split(":")
+    game = int(reg.findall(game)[0])
+    winning, having = nums.split("|")
+    matches = set(reg.findall(winning)) & set(reg.findall(having))
+    GAMES.append((game, matches))
+
+
+def p1():
+    return sum([2 ** (len(nums) - 1) for _, nums in GAMES if len(nums) > 0])
+
+
+def p2():
+    cards = [1] * len(GAMES)
+
+    for game, matches in GAMES:
+        cnt = len(matches)
+        for i in range(game, game + cnt):
+            cards[i] += cards[game - 1]
+
+    return sum(cards)
+
+
+print("1:", p1())
+print("2:", p2())
